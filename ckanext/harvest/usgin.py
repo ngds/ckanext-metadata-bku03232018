f7eab6338ecb8f848ce93b8f09985cb44b146596
asonnenschein <adrian.sonnenschein@gmail.com>
2014-10-08 16:04:36 -0700

    processor for harvested metadata packages, still need to do resources

--- ckanext/harvest/usgin.py
+++ ckanext/harvest/usgin.py
@@ -1,24 +1,11 @@
 import json
-import re
+import datetime
 
 from ckanext.spatial.harvesters import CSWHarvester
 from ckanext.harvest.xml_reader import NgdsXmlMapping
 
 class USGINHarvester(CSWHarvester):
-    '''
-    related_agent = {
-        "agentRole": {
-            "contactAddress": None,
-            "contactEmail": None,
-            "individual": {
-                "personName": None,
-                "personPosition": None,
-            },
-            "organizationName": None,
-            "phoneNumber": None
-        }
-    }
-    '''
+
     def info(self):
         """Return some information about this particular harvester"""
         return {
@@ -101,28 +88,62 @@ class USGINHarvester(CSWHarvester):
         status = {"key": "status", "value": values.get('status', '')}
         extras.append(status)
 
+
         md_package = {}
+
+        harvest_info = md_package["harvestInformation"] = {}
+        harvest_info["version"] = values.get("metadata-standard-version", None)
+        harvest_info["crawlDate"] = datetime.datetime.now().isoformat()
+        harvest_info["indexDate"] = values.get("date-released", None)
+        harvest_info["originalFileIdentifier"] = values.get("guid", None)
+        harvest_info["originalFormat"] = values.get("metadata-standard-name", None)
+        harvest_info["harvestURL"] = ""
+        harvest_info["sourceInfo"] = ""
+        harvest_source = harvest_info["sourceInfo"] = {}
+        harvest_source["harvestSourceID"] = ""
+        harvest_source["viewID"] = ""
+        harvest_source["harvestSourceName"] = ""
+
+        md_properties = md_package["metadataProperties"] = {}
+        mp_contact = md_properties["metadataContact"] = {}
+        mp_agent = mp_contact["relatedAgent"] = {}
+        mp_agent_role = mp_agent["agentRole"] = {}
+        mp_agent_role["agentRoleURI"] = None
+        mp_agent_role["agentRoleLabel"] = None
+        mp_individual = mp_agent_role["individual"] = {}
+        mp_individual["personURI"] = None
+        mp_individual["personName"] = None
+        mp_individual["personPosition"] = None
+        mp_agent["organizationName"] = values.get("contact", None)
+        mp_agent["organizationURI"] = None
+        mp_agent["phoneNumber"] = None
+        mp_agent["contactEmail"] = values.get("contact-email", None)
+        mp_agent["contactAddress"] = None
+
         dates = md_package["citationDates"] = {}
         dates["EventDateObject"] = {}
-        dates["EventDateObject"]["dateTime"] = package_dict.get
+        dates["EventDateObject"]["dateTime"] = values.get("metadata-date", None)
 
-        md_package = {
-            "citationDates": {
-                "EventDateObject": {
-                    "dateTime": None
-                }
-            },
-            "resourceDescription": "",
-            "resourceTitle": "",
-            "resourceContact": {""" related agent here """},
-            "citedSourceAgents": [""" related agents here """],
-            "geographicExtent": [{
-                "eastBoundLongitude": None,
-                "northBoundLatitude": None,
-                "southBoundLatitude": None,
-                "westBoundLongitude": None
-            }]
-        }
+        md_package["resourceDescription"] = values.get("abstract", None)
+        md_package["resourceTitle"] = values.get("title", None)
+
+
+        geo_ext = md_package["geographicExtent"] = {[]}
+        geo_ext[0].append({
+            "eastBoundLongitude": values.get("bbox", None)[0].get("east", None),
+            "northBoundLatitude": values.get("bbox", None)[0].get("north", None),
+            "southBoundLatitude": values.get("bbox", None)[0].get("south", None),
+            "westBoundLongitude": values.get("bbox", None)[0].get("west", None)
+        })
+
+        md_package["citedSourceAgents"] = json.dumps([
+            self.buildRelatedAgent(agent) for agent in values.get('authors', [])
+        ])
+
+        md_package["resourceContact"] = json.dumps([
+            self.buildRelatedAgent(agent) for agent in values.get('maintainers', [])
+        ])
+
 
         md_resource = {
             "accessLinks": {
