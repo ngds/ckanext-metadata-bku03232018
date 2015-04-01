b8d36cdc7c155bd144024512c8dc862098a9d4df
yaditi <ydave@reisys.com>
2015-04-01 14:48:08 -0400

    add usgincm: keyword automatically with tier 3 resource

--- ckanext/metadata/logic/action.py
+++ ckanext/metadata/logic/action.py
@@ -14,6 +14,9 @@ from ckanext.metadata.common import base
 from ckanext.metadata.common import config
 from ckanext.metadata.common import app_globals
 
+import pprint
+import re
+
 log = logging.getLogger(__name__)
 get_action = logic.get_action
 
@@ -337,3 +340,177 @@ def usginmodels_validate_file(context, data_dict):
     log.debug("%s: Finished USGIN content model validation." % resourceName)
 
     return {'valid': valid, 'message': messages, 'dataCorrected': dataCorrected, 'long_fields': long_fields, 'srs': srs, 'resourceName': resourceName, 'resourceId': resourceId}
+
+
+def package_create(context, data_dict):
+    
+    new_package = process_tags(data_dict)
+
+    return logic.action.create.package_create(context, new_package) 
+    
+
+def package_update(context, data_dict):
+    
+    new_package = process_tags(data_dict)
+    
+    return logic.action.update.package_update(context, new_package) 
+
+
+def process_tags(new_package):
+    if 'md_package' in new_package:
+        md_package = json.loads(new_package['md_package'])
+    
+        resourceDescription = md_package.get('resourceDescription', {})
+    
+        uri = resourceDescription.get('usginContentModel', None)
+        version = resourceDescription.get('usginContentModelVersion', None)
+        layer = resourceDescription.get('usginContentModelLayer', None)
+    
+        if None in [uri, version, layer] or 'none' in [uri.lower(), version.lower(), layer.lower()]:
+	        usgin_used = False
+        else:
+            usgin_used = True
+
+        if usgin_used:
+            pp = pprint.PrettyPrinter(indent=4)
+    
+            #remove tags which start with usgincm:
+            remove_tags = []
+            for tags in new_package['tags']:
+                if tags['name'].startswith("usgincm:"):
+                   remove_tags.append(tags)
+            
+            new_package['tags'] = [item for item in new_package['tags'] if item not in remove_tags]          
+            
+            content_models = http_get_content_models()                        
+            
+            for data in content_models:
+                if str(uri).strip() in data['uri'].strip():
+                    title = data['title'].strip()  
+                    break
+            
+            key = title + "+" + layer + "+" + version
+
+            for prefix in get_usgin_prefix(key):
+                if not tag_exists(prefix.decode('UTF-8'), new_package['tags']):
+                    new_package['tags'].append({ 'name': prefix.decode('UTF-8'), 'state': 'active'})
+    
+                    if(new_package['tag_string'] != u''):
+                        tag_string = new_package['tag_string'].split(',')
+                        tag_string = [x for x in tag_string if not x.startswith('usgincm:')]
+                        tag_string.append(prefix)
+                        tag_string = ', '.join([str(x) for x in tag_string])
+                        new_package['tag_string'] = tag_string.decode('UTF-8')
+                    else:
+                        keyword = prefix
+                        new_package['tag_string'] =  keyword.decode('UTF-8')
+            
+    return new_package
+     
+
+def tag_exists(prefix, tags_list):
+    for tags in tags_list:
+        if prefix in tags['name']:
+           return True
+    
+    return False
+
+
+def get_usgin_prefix(key):
+    prefix_map = {
+        
+        "Active Fault/Quaternary Fault+ActiveFault+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/activefault/1.1" : ["usgincm:Quaternary Fault","usgincm:Active Fault"],
+        "Active Fault/Quaternary Fault+ActiveFault+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/activefault/1.2" : ["usgincm:Active Fault1.2"],
+        "Aqueous Chemistry+BaseMetals+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.BaseMetals1.10"],
+        "Aqueous Chemistry+CommonAnalytes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.CommonAnalytes1.10"],
+        "Aqueous Chemistry+FreeGas+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.FreeGas1.10"],
+        "Aqueous Chemistry+GasIsotopes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.GasIsotopes1.10"],
+        "Aqueous Chemistry+IsotopesDissolved+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.IsotopesDissolved1.10"],
+        "Aqueous Chemistry+MajorDissolvedConstituents+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.MajorDissolvedConstituents1.10"],
+        "Aqueous Chemistry+MinorDissolvedConstituents+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.MinorDissolvedConstituents1.10"],
+        "Aqueous Chemistry+Nitrogen+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.Nitrogen1.10"],
+        "Aqueous Chemistry+SingleAnalyte+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.SingleAnalyte1.10"],
+        "Aqueous Chemistry+WaterDissolvedGas+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.WaterDissolvedGas1.10"],
+        "Aqueous Chemistry+WaterIsotopes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.WaterIsotopes1.10"],
+        "Aqueous Chemistry+WaterQuality+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.10" : ["usgincm:Aqueous Chemistry.WaterQuality1.10"],
+        "Aqueous Chemistry+BaseMetals+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.BaseMetals"],
+        "Aqueous Chemistry+CommonAnalytes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.CommonAnalytes"],
+        "Aqueous Chemistry+FreeGas+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.FreeGas"],
+        "Aqueous Chemistry+GasIsotopes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.GasIsotopes"],
+        "Aqueous Chemistry+IsotopesDissolved+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.IsotopesDissolved"],
+        "Aqueous Chemistry+MajorDissolvedConstituents+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.MajorDissolvedConstituents"],
+        "Aqueous Chemistry+MinorDissolvedConstituents+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.MinorDissolvedConstituents"],
+        "Aqueous Chemistry+Nitrogen+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.Nitrogen"],
+        "Aqueous Chemistry+SingleAnalyte+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.SingleAnalyte"],
+        "Aqueous Chemistry+WaterDissolvedGas+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.WaterDissolvedGas"],
+        "Aqueous Chemistry+WaterIsotopes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.WaterIsotopes"],
+        "Aqueous Chemistry+WaterQuality+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/aqueouschemistry/1.9" : ["usgincm:Aqueous Chemistry.WaterQuality"],
+        "Borehole Lithology Intercepts+BoreholeLithIntercept+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/bhlithintercept/1.1" : ["usgincm:Borehole Lithology Intercepts"],
+        "Borehole Lithology Interval Feature+BoreholeLithInterval+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/bhlithinterval/0.9" : ["usgincm:Borehole Lithology Interval Feature"],
+        "Borehole Temperature Observation+BoreholeTemperature+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/boreholetemperature/1.5" : ["usgincm:Borehole Temperature Observation"],
+        "Contour Lines+ContourLine+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/contourlines/1.0" : ["usgincm:Contour Lines"],
+        "Direct Use Feature+DirectUseSite+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/directusesite/1.5" : ["usgincm:Direct Use Feature"],
+        "Drill Stem Test Observations (deprecated)+DrillStemTest+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/drillstemtest/1.8" : ["usgincm:DrillStemTest"],
+        "Fluid Flux Injection and Disposal+FluidFluxInjection+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/fluidfluxinjection/1.1" : ["usgincm:Fluid Flux Injection and Disposal"],
+        "Well Fluid Production+FluidProduction+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/fluidproduction/1.1" : ["usgincm:Well Fluid Production"],
+        "Geologic Reservoir+GeologicReservoir+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geologicreservoir/0.2" : ["usgincm:Geologic Reservoir"],
+        "Geologic Contact Feature+ContactView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-contacts/2.0" : [""],
+        "Geologic Contact Feature+GeologicUnitView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-contacts/2.0" : [""],
+        "Geologic Contact Feature+ShearDisplacementStructureView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-contacts/2.0" : [""],
+        "Geologic Fault Feature / Shear Displacement Structure+Fault+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-faults/1.0" : ["usgincm:Fault Feature"],
+        "Geologic Fault Feature / Shear Displacement Structure+Fault+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-faults/1.1" : ["usgincm:Fault Feature1.1"],
+        "Geologic Fault Feature / Shear Displacement Structure+ContactView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-faults/2.0" : [""],
+        "Geologic Fault Feature / Shear Displacement Structure+GeologicUnitView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-faults/2.0" : [""],
+        "Geologic Fault Feature / Shear Displacement Structure+ShearDisplacementStructureView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-faults/2.0" : [""],
+        "Geologic Units+ContactView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-units/2.0" : ["usgincm:Geologic Contact Feature"],
+        "Geologic Units+GeologicUnitView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-units/2.0" : ["usgincm:Geologic Units"],
+        "Geologic Units+ShearDisplacementStructureView+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geosciml-portrayal-units/2.0" : ["usgincm:Shear Displacement Structure"],
+        "Geothermal Area+GeothermalArea+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geothermalarea/0.5" : ["usgincm:Geothermal Area"],
+        "Geothermal Area+GeothermalArea+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geothermalarea/0.7" : ["usgincm:Geothermal Area0.7"],
+        "Geothermal Fluid Production (deprecated)+GeothermalFluidProduction+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geothermalfluidproduction/1.1" : ["usgincm:Geothermal Fluid Production"],
+        "Powell and Cumming Geothermometry+GasAnalysis+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geothermometry/0.1" : ["usgincm:Powell Cummings Geothermometry.GasAnalysis"],
+        "Powell and Cumming Geothermometry+LiquidAnalysis+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geothermometry/0.1" : ["usgincm:Powell Cummings Geothermometry.LiquidAnalysis"],
+        "Powell and Cumming Geothermometry+GasAnalysis+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geothermometry/2.0" : ["usgincm:Powell Cummings Geothermometry.GasAnalysis2.0"],
+        "Powell and Cumming Geothermometry+LiquidAnalysis+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/geothermometry/2.0" : ["usgincm:Powell Cummings Geothermometry.LiquidAnalysis2.0"],
+        "Gravity Stations+GravityStation+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/gravitystations/0.1" : ["usgincm:Gravity Stations"],
+        "Heat Flow+HeatFlow+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/heatflow/1.15" : ["usgincm:Heat Flow"],
+        "Heat Flow+HeatFlow+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/heatflow/1.23" : ["usgincm:Heat Flow1.23"],
+        "Heat Pump Facility+HeatPumpFacility+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/HeatPumpFacility/0.6" : ["usgincm:Heat Pump Facility"],
+        "Hydraulic Properties+HydraulicProperty+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/hydraulicproperties/1.0" : ["usgincm:Hydraulic Properties"],
+        "Seismic Event Hypocenter+EarthquakeHypocenter+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/hypocenter/1.2.2" : ["usgincm:Seismic Event Hypocenter"],
+        "Seismic Event Hypocenter+EarthquakeHypocenter+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/hypocenter/1.6" : ["usgincm:Seismic Event Hypocenter1.6"],
+        "Seismic Event Hypocenter+Hypocenter+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/hypocenter/1.7" : ["usgincm:Seismic Event Hypocenter1.7"],
+        "Abandoned Mines+AbandonedMine+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/mines/0.3" : ["usgincm:Abandoned Mines"],
+        "Physical Sample+PhysicalSample+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/physicalsample/0.8" : ["usgincm:Physical Sample"],
+        "Geothermal Power Plant Facility+PowerPlantFacility+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/PowerPlantFacility/0.2" : ["usgincm:Geothermal Power Plant Facility"],
+        "Power Plant Production+PlantProduction+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/PowerPlantProduction/0.8" : ["usgincm:Power Plant Production"],
+        "Power Plant Production+PlantProduction+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/PowerPlantProduction/1.0" : ["usgincm:Power Plant Production1.0"],
+        "Radiogenic Heat Production+RadiogenicHeatProduction+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/radiogenicheatproduction/0.5" : ["usgincm:Radiogenic Heat Production"],
+        "Rock Chemistry+Isotopes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.Isotopes"],
+        "Rock Chemistry+NobleGases+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.NobleGases"],
+        "Rock Chemistry+RareEarths+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.RareEarths"],
+        "Rock Chemistry+SingleAnalytes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.SingleAnalytes"],
+        "Rock Chemistry+StableIsotopes+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.StableIsotopes"],
+        "Rock Chemistry+TraceElements+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.TraceElements"],
+        "Rock Chemistry+USeries+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.USeries"],
+        "Rock Chemistry+Volatiles+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.Volatiles"],
+        "Rock Chemistry+WRMajorElements+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/rockchemistry/0.4" : ["usgincm:Rock Chemistry.WRMajorElements"],
+        "Thermal Conductivity Observation+MDThermalConductivity+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/thermalconductivity/1.5" : ["usgincm:Thermal Conductivity Observation"],
+        "Thermal Conductivity Observation+ThermalConductivity+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/thermalconductivity/1.6" : ["usgincm:Thermal Conductivity Observation1.6"],
+        "Thermal Conductivity Observation+ThermalConductivity+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/thermalconductivity/2.0" : ["usgincm:Thermal Conductivity Observation2.0"],
+        "Thermal Conductivity Observation+ThermalConductivity+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/thermalconductivity/2.1" : ["usgincm:Thermal Conductivity Observation2.1"],
+        "Thermal/Hot Spring Feature+ThermalSpring+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/thermalspring/1.6" : ["usgincm:Hot Spring Feature"],
+        "Thermal/Hot Spring Feature+ThermalSpring+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/thermalspring/1.8" : ["usgincm:Hot Spring Feature1.8"],
+        "Volcanic Vents+VolcanicVent+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/volcanicvent/1.0" : ["usgincm:Volcanic Vents"],
+        "Volcanic Vents+VolcanicVent+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/volcanicvent/1.4" : ["usgincm:Volcanic Vents1.4"],
+        "Well Header Observation+Wellheader+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/wellheader/1.5" : ["usgincm:Well Header Observation"],
+        "Well Log Observation+WellLog+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/welllog/0.8" : ["usgincm:Well Log Observation"],
+        "Well Tests+WellTest+http://stategeothermaldata.org/uri-gin/aasg/xmlschema/WellTest/1.0" : ["usgincm:Well Tests"],
+
+    }    
+    
+    return prefix_map.get(key)
+
+
+    
+    
